2ed8345884b41f18eb42137fd17a2448450ca5f9
FrancescoCesari97 <freedus97@gmail.com>
2024-07-19 12:02:35 +0200

    created player sprite

--- code/main.py
+++ code/main.py
@@ -1,4 +1,8 @@
 from settings import * 
+from sprites import *
+
+
+
 
 
 class Game:
@@ -9,6 +13,12 @@ class Game:
         self.clock = pygame.time.Clock()
         self.running = True
 
+        # * sprites
+        self.all_sprites = pygame.sprite.Group()
+        self.paddle_sprites = pygame.sprite.Group()
+        self.player = Player((self.all_sprites, self.paddle_sprites))
+
+
     def run(self):
         while self.running:
             dt = self.clock.tick() / 1000
@@ -17,10 +27,11 @@ class Game:
                     self.running = False
             
             # * update 
-
+            self.all_sprites.update(dt)
 
             # * draw
             self.display_surface.fill(COLORS['bg'])
+            self.all_sprites.draw(self.display_surface)
             pygame.display.update()
         pygame.qiut()
 
